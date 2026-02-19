d3d572b76cafe59eaef92327a06ad620ac161530
Matthew McDonald <matthew.mcdonald@ACSC.cloud>
2026-02-19 03:26:59 +0000

    update tests

--- hatch_build.py
+++ hatch_build.py
@@ -1,19 +1,15 @@
-"""Build retrohunt and yara ready to be bundled for install."""
-
-# hatch_build.py
+import os
 import subprocess
 import sys
-
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
-
 class CustomBuildHook(BuildHookInterface):
-    """Build hook for running actions at build time."""
-
     def initialize(self, version, build_data):
-        """Build retrohunt and yara ready to be bundled for install."""
-        # Runs before build
+        # Skip build steps when running inside tox
+        if os.environ.get("TOX_ENV_NAME"):
+            print("Skipping custom build steps inside tox...")
+            return
+
         print("Running custom build steps...")
-        protoc_command = ["make", "compile"]
-        if subprocess.call(protoc_command) != 0:  # noqa: S603
+        if subprocess.call(["make", "compile"]) != 0:
             sys.exit(-1)
