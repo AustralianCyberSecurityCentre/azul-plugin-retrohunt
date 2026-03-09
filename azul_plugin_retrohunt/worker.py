eba315b860ff4f0024f97dec11a872c00c2d2b07
Matthew McDonald <matthew.mcdonald@ACSC.cloud>
2026-03-09 23:56:02 +0000

    add logs

--- azul_plugin_retrohunt/worker.py
+++ azul_plugin_retrohunt/worker.py
@@ -100,6 +100,7 @@ def _update_progress(job: azm.RetrohuntEvent, logs: StringIO) -> azm.RetrohuntEv
             # Jump to end again
             logs.seek(0, os.SEEK_END)
         job.entity.logs = logs.getvalue()
+    print("updating redis job: ", job)
     rs.redis.set(job.entity.id, json.dumps(job.model_dump()))
     return job
 
@@ -116,7 +117,10 @@ def hunt(index_dirs: list[str], job: azm.RetrohuntEvent, logs: StringIO):
 
     def update_job(phase: int, done: int, total: int, new_match: tuple[str, list[str | bytes]]):
         nonlocal job
-
+        print("Updating job ", job)
+        print("phase ", phase)
+        print("done ", done)
+        print("new_match: ", new_match)
         if phase == SearchPhaseEnum.ATOM_PARSE:
             job.entity.status = azm.HuntState.PARSING_RULES
             job.entity.rules_parsed_total = total
@@ -207,7 +211,7 @@ def hunt(index_dirs: list[str], job: azm.RetrohuntEvent, logs: StringIO):
             # index_dirs = os.path.join(index_dirs, "pcap")
         else:
             raise Exception("Unknown search type.")
-
+        print("Starting search ", search_query, search_enum_type, index_dirs)
         search(
             search_query,
             search_enum_type,
@@ -218,6 +222,7 @@ def hunt(index_dirs: list[str], job: azm.RetrohuntEvent, logs: StringIO):
         )
 
         logger.info("Successfully completed job.")
+        print("job complete")
         job.entity.status = azm.HuntState.COMPLETED
         prom_jobs_run.labels(azm.HuntState.COMPLETED.name).inc()
         logger.debug(job.entity)
@@ -391,7 +396,7 @@ def main():
 
             try:
                 with prom_worker_runtime.time():
-                    logger.debug("Starting hunt")
+                    print("Starting hunt: ", bgi_folders, job, logs)
                     hunt(bgi_folders, job, logs)
                 # Acknowledge the message
                 rs.redis.xack("retrohunt-jobs", "retrohunt-workers", msg_id)
@@ -399,6 +404,7 @@ def main():
                 stop_event.set()
                 rs.redis.delete(f"retrohunt:{job_id}:lock")
         except Exception as e:
+            # Swallow exceptions for tests only.
             if not EXCEPTION_SWALLOWING:
                 raise
             logger.exception(f"Worker error, continuing loop: {e}")
