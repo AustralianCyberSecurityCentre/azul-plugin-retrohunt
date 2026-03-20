734c1c82f386dc2f07df5214745ff810fdfd5ed6
Matthew McDonald <matthew.mcdonald@ACSC.cloud>
2026-03-20 12:22:15 +0000

    another test

--- azul_plugin_retrohunt/worker.py
+++ azul_plugin_retrohunt/worker.py
@@ -356,6 +356,8 @@ def main():
                             print("this is name: ", c["name"])
                             if c["name"].decode() in consumer_name:
                                 print("Found it ", c)
+                                if c["idle"] > LOCK_TTL * 1000:
+                                    print("yes it's time to consume this again")
 
                 result = rs.redis.xautoclaim(
                     "retrohunt-jobs",
