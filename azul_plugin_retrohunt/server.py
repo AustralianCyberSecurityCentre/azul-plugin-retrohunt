72e4e674b43c70ab16877eef752f92f2ac68a224
Matthew McDonald <matthew.mcdonald@ACSC.cloud>
2026-03-29 01:42:40 +0000

    use request.scope['app']

--- azul_plugin_retrohunt/server.py
+++ azul_plugin_retrohunt/server.py
@@ -141,7 +141,7 @@ async def list_hunts(req: Request, limit: int = 100) -> HTMLResponse:
     result = rs.list_hunts(limit)
     ordered_hunts = result["data"]
     print("Hunts ", ordered_hunts)
-    return templates.TemplateResponse("hunts.html", {"request": req, "hunts": ordered_hunts})
+    return templates.TemplateResponse("hunts.html", {"request": req, "hunts": ordered_hunts, "app": app})
 
 
 @app.get("/hunts/{id}", include_in_schema=False)
