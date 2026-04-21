dab0b55a476258076e364cfc01e50ef9dafb61b9
Matthew McDonald <matthew.mcdonald@ACSC.cloud>
2026-04-21 23:49:17 +0000

    linting

--- azul_plugin_retrohunt/retrohunt.py
+++ azul_plugin_retrohunt/retrohunt.py
@@ -149,7 +149,7 @@ class RetrohuntService:
                 status_code=404,
                 detail="There was an issue submitting the hunt.",
             )
-        
+
         try:
             self.redis.xgroup_create(self.RETROHUNT_JOB, self.RETROHUNT_GROUP, id="$", mkstream=True)
         except ResponseError as e:
@@ -162,7 +162,7 @@ class RetrohuntService:
         print("event dict: ", event_dict)
         self.redis.set(retrohunt_id, json.dumps(event_dict))
         self.redis.xadd(self.RETROHUNT_JOB, {"hunt_id": retrohunt_id, "action": "Submitted"})
-        
+
         return retrohunt_id
 
     def run_periodic_tasks(self):
