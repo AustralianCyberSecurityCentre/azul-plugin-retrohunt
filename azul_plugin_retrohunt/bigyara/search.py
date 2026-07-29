691281dab07c2498c856144b2d3b1183bc5114f3
Matthew McDonald <matthew.mcdonald@ACSC.cloud>
2026-07-29 00:27:32 +0000

    fix variable name

--- azul_plugin_retrohunt/bigyara/search.py
+++ azul_plugin_retrohunt/bigyara/search.py
@@ -36,7 +36,6 @@ from .suricata_parse import parse_suricata_rules
 from .yara_parse import RuleSearchPlans, parse_yara_rules
 
 stop_event = Event()
-base_mem = ""
 logger = logging.getLogger("bigyara.search")
 _DURATION_BUCKETS = [0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 1, 5, 10, 30, 60, 120, 300, 600, 1200, 2400]
 
@@ -787,10 +786,10 @@ def clear_stop_event():
 def log_mem():
     """Memory logging."""
     virtual_mem = psutil.virtual_memory()
-    logging.info(f"Total: {virtual_mem.total / (1024 * 1024):.2f} MB")
-    logging.info(f"Available: {virtual_mem.available / (1024 * 1024):.2f} MB")
-    logging.info(f"Used: {virtual_mem.used / (1024 * 1024):.2f} MB")
-    logging.info(f"Percent Used: {virtual_mem.percent}%")
+    logger.info(f"Total: {virtual_mem.total / (1024 * 1024):.2f} MB")
+    logger.info(f"Available: {virtual_mem.available / (1024 * 1024):.2f} MB")
+    logger.info(f"Used: {virtual_mem.used / (1024 * 1024):.2f} MB")
+    logger.info(f"Percent Used: {virtual_mem.percent}%")
 
 
 def _run_suricata(rule_text: str, file_path: str, data: bytes) -> bool:
